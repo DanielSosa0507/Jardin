5ab2abc1620f05ac3dc612d4f44873df94018262
Walter Markerian <waltermarkerian@gmail.com>
2024-06-13 20:38:10 -0300

    Update dbJardin.sql
    Se modifica el modelo de datos.

--- dbJardin.sql
+++ dbJardin.sql
@@ -1,49 +1,168 @@
-use jardin;
+CREATE DATABASE  IF NOT EXISTS `jardin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `jardin`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: jardin
+-- ------------------------------------------------------
+-- Server version	8.0.36
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Crear tabla de géneros
-CREATE TABLE Genero (
-    id_genero INT AUTO_INCREMENT PRIMARY KEY,
-    genero_descripcion VARCHAR(50) NOT NULL
-);
+--
+-- Table structure for table `empleados`
+--
 
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `id_empleado` int NOT NULL AUTO_INCREMENT,
+  `id_rol` int DEFAULT NULL,
+  `id_genero` int DEFAULT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `dni` varchar(20) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `domicilio` varchar(100) DEFAULT NULL,
+  `mail` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_empleado`),
+  KEY `id_rol` (`id_rol`),
+  KEY `id_genero` (`id_genero`),
+  CONSTRAINT `empleados_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`),
+  CONSTRAINT `empleados_ibfk_2` FOREIGN KEY (`id_genero`) REFERENCES `generos` (`id_genero`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Crear tabla de roles
-CREATE TABLE Rol (
-    id_rol INT AUTO_INCREMENT PRIMARY KEY,
-    rol_descripcion VARCHAR(50) NOT NULL
-);
+--
+-- Dumping data for table `empleados`
+--
 
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,1,1,'Juan','Pérez','12345678','1990-05-15','123456789','Calle Principal 123','juan@example.com'),(2,2,2,'María','Gómez','87654321','1988-08-25','987654321','Avenida Central 456','maria@example.com'),(3,3,1,'Pedro','Martínez','13579246','1995-02-10','456123789','Plaza Mayor 789','pedro@example.com'),(4,4,2,'Ana','López','65432198','1992-11-30','987654321','Calle Secundaria 321','ana@example.com'),(5,5,1,'Laura','Rodríguez','98765432','1987-07-20','123456789','Avenida Principal 789','laura@example.com');
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Crear tabla de estados
-CREATE TABLE Estado (
-    id_estado INT AUTO_INCREMENT PRIMARY KEY,
-    estado_descripcion VARCHAR(50) NOT NULL
-);
+--
+-- Table structure for table `estados`
+--
 
--- Crear tabla de empleados
-CREATE TABLE Empleados (
-    id_empleado INT AUTO_INCREMENT PRIMARY KEY,
-    id_rol INT,
-    id_genero INT,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
-    dni VARCHAR(20),
-    fecha_cumpleaños DATE,
-    telefono VARCHAR(20),
-    domicilio VARCHAR(100),
-    mail VARCHAR(100),
-    FOREIGN KEY (id_rol) REFERENCES Rol(id_rol),
-    FOREIGN KEY (id_genero) REFERENCES Genero(id_genero)
-);
+DROP TABLE IF EXISTS `estados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estados` (
+  `id_estado` int NOT NULL AUTO_INCREMENT,
+  `estado_descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_estado`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Crear tabla de relaciones de empleados
-CREATE TABLE Empleados_Relacion (
-    id_empleado INT,
-    id_estado INT,
-    fecha_inicio DATE,
-    fecha_fin DATE,
-    esta_activo BOOLEAN,
-    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado),
-    FOREIGN KEY (id_estado) REFERENCES Estado(id_estado)
-);
+--
+-- Dumping data for table `estados`
+--
+
+LOCK TABLES `estados` WRITE;
+/*!40000 ALTER TABLE `estados` DISABLE KEYS */;
+INSERT INTO `estados` VALUES (1,'Vacaciones'),(2,'Regular'),(3,'Desvinculado'),(4,'Contratado'),(5,'Tercerizado'),(6,'Maternidad'),(7,'Enfermedad');
+/*!40000 ALTER TABLE `estados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `generos`
+--
+
+DROP TABLE IF EXISTS `generos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `generos` (
+  `id_genero` int NOT NULL AUTO_INCREMENT,
+  `genero_descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_genero`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `generos`
+--
+
+LOCK TABLES `generos` WRITE;
+/*!40000 ALTER TABLE `generos` DISABLE KEYS */;
+INSERT INTO `generos` VALUES (1,'Masculino'),(2,'Femenino'),(3,'No binario'),(4,'Prefiero no decirlo'),(5,'Otro');
+/*!40000 ALTER TABLE `generos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `relaciones_laborales`
+--
+
+DROP TABLE IF EXISTS `relaciones_laborales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `relaciones_laborales` (
+  `id_relacion` int unsigned NOT NULL,
+  `id_empleado` int DEFAULT NULL,
+  `id_estado` int DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `esta_activo` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id_relacion`),
+  KEY `id_empleado` (`id_empleado`),
+  KEY `id_estado` (`id_estado`),
+  CONSTRAINT `relaciones_laborales_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleados` (`id_empleado`),
+  CONSTRAINT `relaciones_laborales_ibfk_2` FOREIGN KEY (`id_estado`) REFERENCES `estados` (`id_estado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `relaciones_laborales`
+--
+
+LOCK TABLES `relaciones_laborales` WRITE;
+/*!40000 ALTER TABLE `relaciones_laborales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `relaciones_laborales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id_rol` int NOT NULL AUTO_INCREMENT,
+  `rol_descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Director'),(2,'Maestro'),(3,'Administrativo'),(4,'Maestranza'),(5,'Suplente');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-13 20:37:06
